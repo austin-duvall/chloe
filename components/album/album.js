5a92ac9d5e2936982932248e483d7354b29e95be
Austin Duvall <132301084+austin-duvall@users.noreply.github.com>
2024-01-13 13:30:51 -0800

    stream button jqeury

--- components/album/album.js
+++ components/album/album.js
@@ -1,5 +1,28 @@
 $(document).ready(function() {
   $('#stream').click(function() {
-    $('#streamPanel').slideToggle('slow')
+    $('#streamPanel').slideToggle('slow');
+    $('.btnLink-a').toggleClass('linkClicked');
+    $('.border-x-a').toggleClass('contClicked')
+  })
+  $(document).click(function(e) {
+    if (!$(e.target).closest('#stream').length) {
+      $('#streamPanel').slideUp('slow');
+      $('.btnLink-a').removeClass('linkClicked');
+      $('.border-x-a').removeClass('contClicked');
+    }
+  })
+  let prevScrollPos = $(window).scrollTop();
+
+  $(window).scroll(function() {
+    const scrollThreshold = 5;
+    const currentScrollPos = $(window).scrollTop();
+
+    if (Math.abs(currentScrollPos - prevScrollPos) > scrollThreshold) {
+      $('#streamPanel').slideUp('slow');
+      $('.btnLink-a').removeClass('linkClicked');
+      $('.border-x-a').removeClass('contClicked');
+    }
+
+    prevScrollPos = currentScrollPos;
   })
 })
